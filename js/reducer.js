c777fbfc3a4f85fab52c30311b677eb86f2c3b34
DrewDalmedo <andrewdalmedo@gmail.com>
2020-08-27 08:25:33 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -10,4 +10,13 @@ function changeState(state, action){
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+// calling changeState like this doesn't persist the changes to state made by the function
+// changeState(state, action)
+
+// using a dispatch function can help to persist the changes you want to make to state
+function dispatch(action) {
+  state = changeState(state, action)
+  return state
+}
+
+dispatch(action)
